7f14b24edf2f428042915ac262c718df3456a7b5
alex <ialexies@gmail.com>
2023-03-05 23:53:33 +0800

    Add Drawer for other settings and extra Sign Out Button

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:bmart/app/modules/home/controllers/home_controller.dart';
 import 'package:bmart/app/routes/app_pages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -22,18 +23,49 @@ class HomeView extends GetView<HomeController> {
           child: ListView(
             padding: EdgeInsets.zero,
             children: <Widget>[
-              DrawerHeader(
-                child: Text('Drawer Header'),
+              const DrawerHeader(
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  gradient: LinearGradient(
+                    colors: [Colors.red, Colors.deepOrangeAccent],
+                    stops: [0.0, 0.7],
+                    transform: GradientRotation(90),
+                  ),
                 ),
+                child: Text(
+                  'SETTINGS',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
               ),
               ListTile(
                 title: ElevatedButton(
                   onPressed: () {
                     Get.toNamed(Routes.profile);
                   },
-                  child: Text('Profile'),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.deepOrange)),
+                  child: const Text('My Profile'),
+                ),
+                onTap: controller.closeDrawer,
+              ),
+              ListTile(
+                title: ElevatedButton(
+                  onPressed: () async {
+                    // Get.toNamed(Routes.profile);
+                    await FirebaseAuth.instance.signOut();
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.deepOrange)),
+                  child: const Text('Logout'),
                 ),
                 onTap: controller.closeDrawer,
               )
