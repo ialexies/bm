900e037a727bec19136468b2ab01b3b779838898
alex <ialexies@gmail.com>
2023-03-06 09:17:25 +0800

    Refactor Tab, Add badges with counter

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,10 @@
 import 'package:bmart/app/modules/bmartSidebar/views/bmart_sidebar_view.dart';
 import 'package:bmart/app/modules/home/controllers/home_controller.dart';
 import 'package:bmart/app/routes/app_pages.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
 
 import 'package:get/get.dart';
 
@@ -12,8 +14,12 @@ class HomeView extends GetView<HomeController> {
   TabBar get _tabBar => const TabBar(
         indicatorColor: Colors.amber,
         tabs: [
-          Tab(icon: Icon(Icons.call)),
-          Tab(icon: Icon(Icons.message)),
+          Tab(
+            child: _TabBarEach('TAB A ', 5),
+          ),
+          Tab(
+            child: _TabBarEach('TAB B ', 5),
+          ),
         ],
       );
 
@@ -32,10 +38,34 @@ class HomeView extends GetView<HomeController> {
             ),
           ),
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: [
-            Icon(Icons.directions_transit),
-            Icon(Icons.directions_bike),
+            Align(
+              alignment: Alignment.topCenter,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 200,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 5),
+                ),
+                items: [1, 2, 3, 4, 5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        decoration: const BoxDecoration(color: Colors.amber),
+                        child: Text(
+                          'text $i',
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+            ),
+            const Icon(Icons.directions_bike),
           ],
         ),
         drawer: Drawer(
@@ -52,8 +82,9 @@ class HomeView extends GetView<HomeController> {
                     Get.toNamed(Routes.profile);
                   },
                   style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.deepOrange)),
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.deepOrange),
+                  ),
                   child: const Text('My Profile'),
                 ),
                 onTap: controller.closeDrawer,
@@ -75,6 +106,38 @@ class HomeView extends GetView<HomeController> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _TabBarEach extends StatelessWidget {
+  const _TabBarEach(this._title, this._count);
+
+  final String _title;
+  final int _count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(_title),
+        badges.Badge(
+          badgeStyle: const badges.BadgeStyle(
+            badgeColor: Colors.black,
+          ),
+          badgeContent: Text(
+            _count.toString(),
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          child: const Icon(
+            Icons.image,
+            color: Colors.white,
+          ),
+        )
+      ],
     );
   }
 }
